6eb3dacc2eeb42e4872b9e45ffd2d7583828eb69
Debprateem <70195817+Debprateem@users.noreply.github.com>
2020-10-27 22:07:51 +0530

    Add files via upload

---
+++ SQLActivity1/ACTIVITY1.sql
@@ -0,0 +1,4 @@
+select * from employees.customers;
+create database Debprateem_activities;
+use Debprateem_activities; 
+
---
+++ SQLActivity1/ACTIVITY4.sql
@@ -0,0 +1,39 @@
+select * from employees.customers;
+create database Debprateem_activities;
+use Debprateem_activities; 
+
+-- Create salesman table
+CREATE TABLE salesman (salesman_id int primary key, name varchar(32), city varchar(32), commission int);
+
+-- Insert values one at a time
+insert into salesman values(5001, 'James Hoog', 'New York', 15);
+
+-- Insert multiple rows at once
+insert into	salesman values
+	(5002, 'Nail Knite', 'Paris', 13), 
+	(5005, 'Pit Alex', 'London', 11), 
+    (5006, 'McLyon', 'Paris', 14), 
+    (5007, 'Paul Adam', 'Rome', 13),
+    (5003, 'Lauson Hen', 'San Jose', 12);
+
+-- View all columns
+select * from salesman;
+
+-- Show data from the salesman_id and city columns
+select salesman_id, city from salesman;
+
+-- Show data of salesman from Paris
+select * from salesman where city='Paris';
+
+-- Show salesman_id and commission of Paul Adam
+select salesman_id, commission from salesman where name='Paul Adam';
+
+
+-- Add the grade column
+Alter table salesman add grade int;
+
+-- Update the values in the grade column
+update salesman set grade=100;
+
+-- Display data
+select * from salesman;
